b40968987b7e607c49f0e0cb4c52a0549f8d7039
jiaorengui <tiny.jiao@aliyun.com>
2019-09-26 18:35:24 +0800

    update

---
+++ lib/views/login/index.dart
@@ -0,0 +1,21 @@
+/*
+ * @Author: Tiny
+ * @Date: 2019-09-26 16:48:12
+ * @Last Modified by: tiny.jiao@aliyun.com
+ * @Last Modified time: 2019-09-26 18:33:38
+ */
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Image.asset('../../images/flutterlog.svg')
+          ]
+        ),
+      ),
+    );
+  }
+}
